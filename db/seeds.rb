a70742b3cfbbf83f087be4411136d3bd134a3dc6
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-10 14:59:11 +0500

    Successfully seed the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do |blog|
+    Blog.create(
+    title:"My Blog #{blog}",
+    body:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). #{blog}")
+end
+
+10.times do |skill|
+    Skill.create(
+        title: "Skill#{skill}",
+        percent_utilized: 15+skill
+    )
+end
+
+5.times do |portfolio|
+    Portfolio.create(
+        title:"Portfolio#{portfolio}",
+        subtitle:"SubPortfolio#{portfolio}",
+        body:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.#{portfolio}",
+        main_image: "https://place-hold.it/300",
+        thumb_image: "https://place-hold.it/300"
+    )
+end
